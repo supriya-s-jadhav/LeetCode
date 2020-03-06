4dc945f1d5a9927da14374b6ba31d69a9add57fa
supriya-s-jadhav <supria005@gmail.com>
2020-03-06 16:39:49 -0600

    solution updated

--- TenthLine/TenthLine.sh
+++ TenthLine/TenthLine.sh
@@ -3,11 +3,11 @@
 # This bash script reads file.txt file and prints just the 10th line of the file.
 
 if [ -f file.txt ]; then
-    echo "file.txt exist"
+    #echo "file.txt exist"
     file_len=($(wc -l < file.txt))
-    echo $file_len
+    #echo $file_len
     if [ $file_len -lt 10 ]; then
-        echo "file.txt has less than 10 lines."
+        #echo "file.txt has less than 10 lines."
         exit 0
     else
         echo "$(head -n 10 file.txt | tail -n +10)"
