25ed842604c573f8a059d1e693267d1b951617f0
supriya-s-jadhav <supria005@gmail.com>
2020-03-10 17:23:31 -0500

    Transpose file script

--- Valid Phone Numbers/validphonenumbers.sh
+++ Valid Phone Numbers/validphonenumbers.sh
@@ -2,5 +2,5 @@
 
 # This script will print the valid phone numbers from  file.txt file. The file.txt file has list of phone numbers, one phone number per line. The valid phone number format is (xxx) xxx-xxxx or xxx-xxx-xxxx where x means digit.
 
-cat file.txt | grep -E '^(:?[0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$'
+if [ file.txt ]; then cat file.txt | grep -E '^(:?[0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$'; fi
 
