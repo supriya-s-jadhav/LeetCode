7505340ea57adde8c95c7be1de5d5e5d3835bdc9
supriya-s-jadhav <supria005@gmail.com>
2020-03-12 15:00:18 -0500

    updated

--- 192.Word Frequency/wordfrequency.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script reads teh file.txt text file and prints the word count
-
-cat words.txt | xargs -n1 | sort | uniq -c | sort -nr | awk '{print $2 " " $1}'
--- Valid Phone Numbers/validphonenumbers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script will print the valid phone numbers from  file.txt file. The file.txt file has list of phone numbers, one phone number per line. The valid phone number format is (xxx) xxx-xxxx or xxx-xxx-xxxx where x means digit.
-
-if [ file.txt ]; then cat file.txt | grep -E '^(:?[0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$'; fi
-
